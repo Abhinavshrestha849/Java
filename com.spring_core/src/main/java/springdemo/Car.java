9d792fe897533da50c2dd69b6ee92de3c8f304a5
abhinavshrestha <abhinav.shrestha.lal.23@gmail.com>
2024-02-21 09:07:58 -0600

    Initial Commit

---
+++ com.spring_core/src/main/java/springdemo/Car.java
@@ -0,0 +1,28 @@
+package springdemo;
+
+public class Car {
+	
+	private String color;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	private String brand;
+	private int price;
+	
+
+}
